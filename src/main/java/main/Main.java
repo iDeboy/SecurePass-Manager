b6141863676c759b7961b97c7a98f2a05513b87e
Laura Espejo Alvarado <lauraespejo08@gmail.com>
2023-11-12 20:59:44 -0600

    Creación de ventana Login

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,11 +1,17 @@
 package main;
 
+import views.Login;
+
 public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hola, Mundo!");
-
+		java.awt.EventQueue.invokeLater(new Runnable() {
+                    public void run() {
+                        new Login().setVisible(true);
+                    }
+                });
+                
 	}
 
 }
