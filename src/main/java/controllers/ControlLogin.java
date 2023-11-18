d40ac1fce6521d8b151354b9845aa15b6f84e7f8
Laura Espejo Alvarado <lauraespejo08@gmail.com>
2023-11-18 15:12:09 -0600

    Implementación de MauseListener en ViewLogin

--- src/main/java/controllers/ControlLogin.java
+++ src/main/java/controllers/ControlLogin.java
@@ -5,6 +5,10 @@
 package controllers;
 
 import com.formdev.flatlaf.FlatLightLaf;
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
@@ -15,12 +19,17 @@ import views.ViewLogin;
  *
  * @author laura
  */
-public class ControlLogin{ //implements ActionListener, MouseListener
+public class ControlLogin implements MouseListener{ //implements ActionListener, MouseListener
 
     private ViewLogin viewLogin;
+    private boolean iconoAlternativo = false;
 
     public ControlLogin(ViewLogin viewLogin) {
         this.viewLogin = viewLogin;
+        
+        this.viewLogin.txtNomUsuario.addMouseListener(this);
+        this.viewLogin.pfPassword.addMouseListener(this);
+        this.viewLogin.jlbIco3.addMouseListener(this);
     }
     
     public void iniciarVistaLogin() throws UnsupportedLookAndFeelException{
@@ -29,6 +38,8 @@ public class ControlLogin{ //implements ActionListener, MouseListener
         viewLogin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         viewLogin.setLocation(250, 90);
         viewLogin.setResizable(false);
+        
+        viewLogin.pfPassword.setEchoChar((char) 0);
         
         UIManager.setLookAndFeel(new FlatLightLaf());
         
@@ -39,5 +50,68 @@ public class ControlLogin{ //implements ActionListener, MouseListener
         SwingUtilities.updateComponentTreeUI(viewLogin);
         viewLogin.setVisible(true);
     }
+
     
+    @Override
+    public void mousePressed(MouseEvent e) {
+        if(viewLogin.txtNomUsuario == e.getSource()){
+            
+            if(viewLogin.txtNomUsuario.getText().equals("Usuario")){
+                viewLogin.txtNomUsuario.setText("");
+                viewLogin.txtNomUsuario.setForeground(Color.black);
+            }
+            if(String.valueOf(viewLogin.pfPassword.getPassword()).isEmpty()){
+                viewLogin.pfPassword.setText("Contraseña");
+                viewLogin.pfPassword.setEchoChar((char) 0);
+                viewLogin.pfPassword.setForeground(new Color(102, 102, 102));
+            }
+            
+        }
+        if(viewLogin.pfPassword == e.getSource()){
+            
+            if(String.valueOf(viewLogin.pfPassword.getPassword()).equals("Contraseña")){
+                viewLogin.pfPassword.setText("");
+                viewLogin.pfPassword.setEchoChar('\u2022');
+                viewLogin.pfPassword.setForeground(Color.black);
+            }
+            if(viewLogin.txtNomUsuario.getText().isEmpty()){
+                viewLogin.txtNomUsuario.setText("Usuario");
+                viewLogin.txtNomUsuario.setForeground(new Color(102, 102, 102));
+            }
+            
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if(viewLogin.jlbIco3 == e.getSource()){
+            
+            if(iconoAlternativo){
+                viewLogin.jlbIco3.setIcon(new ImageIcon(getClass().getResource("/icons/eyeON.png")));
+                viewLogin.pfPassword.setEchoChar((char) 0);
+            }
+            else{
+                viewLogin.jlbIco3.setIcon(new ImageIcon(getClass().getResource("/icons/eyeOFF.png")));
+                viewLogin.pfPassword.setEchoChar('\u2022');
+            }
+            
+            iconoAlternativo = !iconoAlternativo;
+        }
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+      
 }
--- src/main/java/views/ViewLogin.java
+++ src/main/java/views/ViewLogin.java
@@ -5,8 +5,12 @@
 package views;
 
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.font.TextAttribute;
+import java.util.HashMap;
+import java.util.Map;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -77,20 +81,27 @@ public class ViewLogin extends JFrame{
         jlbRegistro.setText("Regístrate");
         jlbRegistro.setFont(new Font("Roboto", 0, 16));
         jlbRegistro.setForeground(new Color(255, 255, 255));
+        jlbRegistro.setCursor(new java.awt.Cursor(Cursor.HAND_CURSOR));
+        
+        Font fuenteActual = jlbRegistro.getFont();
+        Map<TextAttribute, Object> atributos = new HashMap<>(fuenteActual.getAttributes());
+        atributos.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+        Font fuenteSubrayada = fuenteActual.deriveFont(atributos);
+        jlbRegistro.setFont(fuenteSubrayada);
         
         //JTextField
         txtNomUsuario = new JTextField();
         txtNomUsuario.setText("Usuario");
         txtNomUsuario.setFont(new Font("Roboto Mono", 0, 15));
         txtNomUsuario.setForeground(new Color(102, 102, 102));
-        txtNomUsuario.setPreferredSize(new Dimension(260, 44));
+        txtNomUsuario.setPreferredSize(new Dimension(300, 44));
         
         //JPasswordField
         pfPassword = new JPasswordField();
         pfPassword.setText("Contraseña");
         pfPassword.setFont(new Font("Roboto Mono", 0, 15));
         pfPassword.setForeground(new Color(102, 102, 102));
-        pfPassword.setPreferredSize(new Dimension(260, 44));
+        pfPassword.setPreferredSize(new Dimension(300, 44));
         
         //JButton
         bEntrar = new JButton();
@@ -100,6 +111,7 @@ public class ViewLogin extends JFrame{
         bEntrar.setBackground(new Color(141, 174, 236));
         bEntrar.setBorder(null);
         bEntrar.setPreferredSize(new Dimension(227, 44));
+        bEntrar.setCursor(new Cursor(java.awt.Cursor.HAND_CURSOR));
         
         //Diseño
         PanelPrincipal.add(jlbImg1, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 0, -1, -1));
@@ -109,12 +121,12 @@ public class ViewLogin extends JFrame{
         PanelPrincipal.add(jlbTitulo, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 220, -1, -1));
         PanelPrincipal.add(jlbIco1, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 290, -1, -1));
         PanelPrincipal.add(jlbIco2, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 360, -1, -1));
-        PanelPrincipal.add(jlbIco3, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 360, -1, -1));
-        PanelPrincipal.add(jlbPregunta, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 570, -1, -1));
-        PanelPrincipal.add(jlbRegistro, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 570, -1, -1));
+        PanelPrincipal.add(jlbIco3, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 360, -1, -1));
+        PanelPrincipal.add(jlbPregunta, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 570, -1, -1));
+        PanelPrincipal.add(jlbRegistro, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 570, -1, -1));
         PanelPrincipal.add(txtNomUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 280, -1, -1));
         PanelPrincipal.add(pfPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 350, -1, -1));
-        PanelPrincipal.add(bEntrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 450, -1, -1));
+        PanelPrincipal.add(bEntrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 450, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
