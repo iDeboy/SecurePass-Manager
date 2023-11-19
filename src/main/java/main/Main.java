bd1c327a574af2c04c5684be3132f6f565d96939
Laura Espejo Alvarado <lauraespejo08@gmail.com>
2023-11-19 15:27:48 -0600

    Vistas para la edición y visualización de datos en las credenciales

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import controllers.ControlCredencial;
 import views.ViewLogin;
 import controllers.ControlLogin;
 import controllers.ControlRegistro;
@@ -7,6 +8,8 @@ import javax.swing.UnsupportedLookAndFeelException;
 import views.ViewPrincipal;
 import views.ViewRegistro;
 import controllers.ControlPrincipal;
+import views.ViewCredencialB;
+import views.ViewCredencialP;
 import views.ViewTipoCreden;
 
 public class Main {
@@ -17,10 +20,14 @@ public class Main {
         ViewTipoCreden viewTipoCreden = new ViewTipoCreden();
         ViewPrincipal viewPrincipal = new ViewPrincipal();
         ViewRegistro viewRegistro = new ViewRegistro();
-        
+//        ViewCredencialP viewCredencialP  = new ViewCredencialP();
+//        ViewCredencialB viewCredencialB = new ViewCredencialB();
+
         ControlLogin controlLogin = new ControlLogin(viewLogin, null,null);
         ControlRegistro controlRegistro = new ControlRegistro(viewRegistro, controlLogin,null);
         ControlPrincipal controlPrincipal = new ControlPrincipal(viewPrincipal,controlLogin,controlRegistro,viewTipoCreden);
+//        ControlCredencial controlCredencial = new ControlCredencial(viewCredencial,controlPrincipal);
+//        ControlCredencial controlCredencial = new ControlCredencial(viewCredencialP,controlPrincipal,viewCredencialB);
         
         controlLogin.setControlRegistro(controlRegistro);
         controlLogin.setControlPrincipal(controlPrincipal);
